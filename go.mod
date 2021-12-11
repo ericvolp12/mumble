6f46be07411eb4c51b69a00352072f426f16eea9
Eric Volpert <ericvolp12@gmail.com>
2021-12-11 15:45:38 -0800

    It works! As long as only one person is talking in the other room

--- go.mod
+++ go.mod
@@ -1,17 +1,20 @@
-module github.com/bwmarrin/discordgo/examples/voice_receive
+module github.com/ericvolp12/mumble
 
 go 1.17
 
 require (
 	github.com/bwmarrin/dgvoice v0.0.0-20210225172318-caaac756e02e
 	github.com/bwmarrin/discordgo v0.23.2
+	github.com/eripe970/go-dsp-utils v0.0.0-20210713145451-565b93ef3ca1
 	github.com/mattetti/audio v0.0.0-20190404201502-c6aebeb78429
+	github.com/moutend/go-equalizer v0.1.0
 )
 
 require (
+	github.com/goccmack/godsp v0.1.1 // indirect
+	github.com/goccmack/goutil v0.4.0 // indirect
 	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/mjibson/go-dsp v0.0.0-20180508042940-11479a337f12 // indirect
-	github.com/moutend/go-equalizer v0.1.0 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
--- main.go
+++ main.go
@@ -1,36 +1,37 @@
 package main
 
 import (
-	"encoding/binary"
 	"flag"
 	"fmt"
 
 	"github.com/bwmarrin/dgvoice"
 	"github.com/bwmarrin/discordgo"
-	"github.com/mattetti/audio"
-	"github.com/moutend/go-equalizer/pkg/equalizer"
+	dsp "github.com/eripe970/go-dsp-utils"
 )
 
 func main() {
 
 	// NOTE: All of the below fields are required for this example to work correctly.
 	var (
-		Token     = flag.String("t", "", "Discord bot token.")
-		GuildID   = flag.String("g", "", "Guild ID")
-		ChannelID = flag.String("c", "", "Channel ID")
-		err       error
+		Token1     = flag.String("t1", "", "Discord bot 1 token.")
+		Token2     = flag.String("t2", "", "Discord bot 2 token.")
+		GuildID    = flag.String("g", "", "Guild ID")
+		ChannelID1 = flag.String("c1", "", "Channel ID 1")
+		ChannelID2 = flag.String("c2", "", "Channel ID 2")
+		err        error
 	)
 	flag.Parse()
 
+	// Initialize Bot 1
 	// Connect to Discord
-	discord, err := discordgo.New("Bot " + *Token)
+	discord1, err := discordgo.New("Bot " + *Token1)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	// Open Websocket
-	err = discord.Open()
+	err = discord1.Open()
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -38,67 +39,108 @@ func main() {
 
 	// Connect to voice channel.
 	// NOTE: Setting mute to false, deaf to true.
-	dgv, err := discord.ChannelVoiceJoin(*GuildID, *ChannelID, false, false)
+	dgv1, err := discord1.ChannelVoiceJoin(*GuildID, *ChannelID1, false, false)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Initialize Bot 2
+	// Connect to Discord
+	discord2, err := discordgo.New("Bot " + *Token2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Open Websocket
+	err = discord2.Open()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Connect to voice channel.
+	// NOTE: Setting mute to false, deaf to true.
+	dgv2, err := discord2.ChannelVoiceJoin(*GuildID, *ChannelID2, false, false)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	// Starts echo
-	echo(dgv)
+	echo(dgv1, dgv2)
 
 	// Close connections
-	dgv.Close()
-	discord.Close()
+	dgv1.Close()
+	discord1.Close()
 
-	return
+	dgv2.Close()
+	discord2.Close()
 }
 
 // Takes inbound audio and sends it right back out.
-func echo(v *discordgo.VoiceConnection) {
+func echo(v1 *discordgo.VoiceConnection, v2 *discordgo.VoiceConnection) {
 
-	recv := make(chan *discordgo.Packet, 2)
-	go dgvoice.ReceivePCM(v, recv)
+	recv1 := make(chan *discordgo.Packet, 2)
+	go dgvoice.ReceivePCM(v1, recv1)
 
-	send := make(chan []int16, 2)
-	go dgvoice.SendPCM(v, send)
+	send1 := make(chan []int16, 2)
+	go dgvoice.SendPCM(v1, send1)
 
-	v.Speaking(true)
-	defer v.Speaking(false)
+	v1.Speaking(true)
+	defer v1.Speaking(false)
+
+	recv2 := make(chan *discordgo.Packet, 2)
+	go dgvoice.ReceivePCM(v2, recv2)
+
+	send2 := make(chan []int16, 2)
+	go dgvoice.SendPCM(v2, send2)
+
+	v2.Speaking(true)
+	defer v2.Speaking(false)
 
 	for {
 
-		p, ok := <-recv
+		p, ok := <-recv1
 		if !ok {
 			return
 		}
 
-		rawInts := []int{}
+		normalizedFloats := []float64{}
 
 		for _, i := range p.PCM {
-			rawInts = append(rawInts, int(i))
+			newFloat := float64(i) / 32768.0
+			if newFloat > 1 {
+				newFloat = 1
+			}
+			if newFloat < -1 {
+				newFloat = -1
+			}
+			normalizedFloats = append(normalizedFloats, newFloat)
 		}
 
-		buf := audio.NewPCMIntBuffer(rawInts, &audio.Format{
-			NumChannels: 1,
-			SampleRate:  48000,
-			BitDepth:    16,
-			Endianness:  binary.LittleEndian,
-		})
-
-		buf.SwitchPrimaryType(audio.Float)
-
-		outs := []float64{}
-
-		bpf := equalizer.NewLowPass(48000, 80, 0.5)
-		for _, i := range buf.AsFloat64s() {
-			outs = append(outs, bpf.Apply(i))
+		inputSignal := &dsp.Signal{
+			SampleRate: 48000,
+			Signal:     normalizedFloats,
 		}
 
-		buf.Floats = outs
+		signalFiltered, err := inputSignal.LowPassFilter(80)
+		if err != nil {
+			fmt.Printf("Failed to low-pass: %v", err.Error())
+		}
 
-		buf.SwitchPrimaryType(audio.Integer)
+		outs := []int16{}
 
-		send <- buf.AsInt16s()
+		for _, i := range signalFiltered.Signal {
+			scaled := int16(i * 32768)
+			if scaled > 32767 || scaled < -32767 {
+				fmt.Printf("Got weird value: %v\n", scaled)
+			}
+			outs = append(outs, scaled)
+		}
+
+		send2 <- outs
+
 	}
 }
